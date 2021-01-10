2f1a91dc1ec6b157e013513bfe36ef546b406840
Rida Ayed <rida.ayed@notebox.ch>
2021-01-10 17:26:16 +0100

    adapt curl command, add (expanded) script content

--- hugo_shortcode_gallery_generator.sh
+++ hugo_shortcode_gallery_generator.sh
@@ -1,1 +1,134 @@
-<<hugo_shortcode_script>>
+#!/bin/sh
+if [ -z "$1" ]
+then
+    OUT_DIR=/tmp
+else
+    OUT_DIR=$1
+fi
+if [ -z "$2" ]
+then
+    SITE=hugo
+else
+    SITE=$2
+fi
+
+echo "Check if $OUT_DIR does not exist"
+if [ ! -d "$OUT_DIR" ]
+then
+    echo "Directory $1 DOES NOT exists. You can create it using: mkdir $1"
+    exit 9999 # die with error code 9999
+fi
+
+echo "Working directory is $OUT_DIR"
+
+echo "Sitename is $SITE"
+cd $OUT_DIR
+
+echo "Kill any running hugo server"
+sudo killall -s SIGKILL hugo
+
+echo "Remove any remnants from previous tests by \
+uncommenting this line in the script or run it manually:"
+sudo rm -r -f $SITE
+
+echo "Generate a new site with $SITE"
+hugo new site $SITE
+
+echo "Get a lightweight theme"
+cd $OUT_DIR/$SITE
+git init
+git submodule add https://github.com/de-souza/hugo-flex.git themes/hugo-flex
+# git submodule add https://github.com/yihui/hugo-xmin themes/hugo-xmin
+
+echo "Add the theme to the site's config"
+
+echo 'theme = "hugo-flex"' >> config.toml
+# echo 'theme = "hugo-xmin"' >> config.toml
+
+echo "Create a first post"
+hugo new posts/my-first-post.md
+
+echo "Show what has been created"
+cat ~$OUT_DIR/$SITE/content/posts/my-first-post.md
+
+echo "Add the word 'foobar' and some others to the post"
+printf '# foobar\nbar\n- lorem\n- ipsum' >> $OUT_DIR/$SITE/content/posts/my-first-post.md
+
+echo "Start hugo server and build $SITE"
+hugo server --buildDrafts &
+nohup hugo server --buildDrafts &
+
+echo "Do a test: \
+Wait some time for hugo to generate the server, \
+then get the website and assert it contains the word foo in it's html source \
+by grepping it in the html source \
+(you might have to do this manually if $SITE is cluttering your terminal"
+sleep 3s
+
+echo `curl -s http://localhost:1313/ | grep 'foobar'`
+
+echo "Kill any running hugo server"
+sudo killall -s SIGKILL hugo
+
+echo "Remove any remnants from previous tests by \
+uncommenting this line in the script or run it manually:"
+sudo rm -r -f $OUT_DIR/$SITE/themes/hugo-shortcode-gallery
+
+echo "Cloning the hugo-shortcode-gallery git repository into $OUT_DIR/$SITE/themes."
+cd $OUT_DIR/$SITE/themes
+git clone https://github.com/mfg92/hugo-shortcode-gallery.git
+
+echo "Adding this theme to $OUT_DIR/$SITE/config.toml using GNU sed"
+
+echo `sed --debug -i 's/"hugo-flex"/\["hugo-flex", "hugo-shortcode-gallery"\]/' $OUT_DIR/$SITE/config.toml`
+# echo `sed --debug -i 's/"hugo-xmin"/\["hugo-xmin", "hugo-shortcode-gallery"\]/' $OUT_DIR/$SITE/config.toml`
+
+echo "Adding the gallery to the navigation in $OUT_DIR/$SITE/config.toml"
+printf "
+[[menu.nav]]
+name = '"Gallery"'
+url = '"gallery/"'
+weight = 1
+" >> $OUT_DIR/$SITE/config.toml
+
+echo "Create the gallery and images folder"
+mkdir --parents $OUT_DIR/$SITE/content/gallery/images
+
+echo "Create the index.md in the gallery folder"
+cd $OUT_DIR/$SITE
+hugo new /gallery/index.md
+
+echo "Check content of index.md"
+cat $OUT_DIR/$SITE/content/gallery/index.md
+
+echo "Download some images into gallery/images"
+cd $OUT_DIR/$SITE/content/gallery/images
+wget --no-verbose https://raw.githubusercontent.com/mfg92/hugo-shortcode-gallery/example_site/example_site/content/gallery/images/2020-06-18-_MG_9066.jpg
+wget --no-verbose https://raw.githubusercontent.com/mfg92/hugo-shortcode-gallery/example_site/example_site/content/gallery/images/2020-07-26-_MG_3439.jpg
+
+echo "Adding the shortcode to index.md"
+printf 'Here is an example gallery:
+
+{{< gallery
+    match="images/*"
+    showExif="true"
+    sortOrder="desc"
+    loadJQuery="true"
+    embedPreview="true"
+>}}' >> $OUT_DIR/$SITE/content/gallery/index.md
+
+echo "cd into $OUT_DIR/$SITE and build the site using hugo"
+cd $OUT_DIR/$SITE
+nohup hugo server --buildDrafts &
+
+echo "Do a test: \
+Wait some time for hugo to generate the server, \
+then visit the gallery and assert it contains the string 'jpg', \
+by grepping it in the html source \
+(you might have to do this manually if hugo is cluttering your terminal"
+sleep 3s
+
+echo `curl -s http://localhost:1313/gallery/ | grep 'jpg'`
+
+echo "Visit http://localhost:1313/gallery/ using your default browser"
+xdg-open http://localhost:1313/gallery/
